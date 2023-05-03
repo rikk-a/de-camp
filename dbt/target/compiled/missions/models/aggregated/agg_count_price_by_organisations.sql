b972f4c4fbea3e94b05ae89e2327fa84bf837952
Viktoria K <rikk_a@bk.ru>
2023-05-03 20:52:17 +0300

    removed logs

--- dbt/target/compiled/missions/models/aggregated/agg_count_price_by_organisations.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    organisation
-,   mission_status
-,   count(*) as count_by_status
-,   round(sum(price),6) as price
-FROM
-    `de-camp-v1`.`mission_launches_dataset`.`stg_space_mission_launches__mission_launches`
-group by organisation , mission_status
-order by organisation,mission_status
--- dbt/target/compiled/missions/models/aggregated/avg_price_by_year.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    date_trunc(date, YEAR)     as `year`
-,   round(avg(price),6)        as avg_price
-FROM
-    `de-camp-v1`.`mission_launches_dataset`.`stg_space_mission_launches__mission_launches`
-group by date
-order by date
